45cbfd1a4b7064eabe229e8c98619fb20ad1045d
MoshkaIO <137087709+MoshkaIO@users.noreply.github.com>
2024-05-03 12:43:21 +0300

    5&6 LR

---
+++ src/main/java/com/example/springboot/contollers/SampleController.java
@@ -0,0 +1,15 @@
+package com.example.springboot.contollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SampleController {
+
+    @GetMapping("/title")
+    public String getTitle() {
+        return "<title>HELLO FROM BACKEND</title>";
+    }
+}
---
+++ src/main/java/com/example/springboot/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.springboot.repositories;
+
+import com.example.springboot.models.Artist;
+//import com.example.springboot.models.Country;
+import com.example.springboot.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long>
+{
+
+}
